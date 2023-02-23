d34d2406c399248040d285c46848a542f19b0316
abdevworks <czasacz@gmail.com>
2023-02-23 08:01:28 +0100

    feat: movie
    Added rate movie dialog view

--- ale-kino/src/app/user/features/home/movie/movie.component.ts
+++ ale-kino/src/app/user/features/home/movie/movie.component.ts
@@ -1,3 +1,4 @@
+import { UserStateService } from 'src/app/core/user.state.service';
 import { AuthStateService } from 'src/app/auth/auth.state.service';
 import { Component, OnInit, Input, inject } from '@angular/core';
 import { DailyMovieScreenings } from 'src/app/user/features/home/movie/movie.interface';
@@ -11,6 +12,8 @@ import { MovieScoreDialogComponent } from '../movie-score-dialog/movie-score-dia
 })
 export class MovieComponent implements OnInit {
   private authStateService = inject(AuthStateService);
+  private userStateService = inject(UserStateService);
+
   @Input() movie!: DailyMovieScreenings;
 
   constructor(public dialog: MatDialog) {}
